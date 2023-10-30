62a26669c8375aff403114de0a67c7a1d16293f4
Aakash106848 <aakash.b@gavstech.com>
2023-10-30 10:12:02 +0530

    Project Commit

---
+++ src/app/UserDetails.ts
@@ -0,0 +1,7 @@
+export class UserData   {
+    userslist:Array<object>=[];
+    FirstName:string = '';
+    LastName:string = '';
+    Location:string = 'Chennai';
+    Company:string = 'Gavs';
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
-
+import { UserData } from './UserDetails';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  udata:UserData =new UserData();
+
   title = 'binding';
+  ThemeName:string = ''
+  ThemeName1:string = ''
+  textboxcolor:string = ''
+  applycolor:string = ''
+  size:number =0
+  FirstName:string = ''
+  LastName:string = ''
+  enabledisable:boolean = false
+  enabledisable1:boolean =false
+  FirstName1:string = ''
+  LastName1:string = ''
+  ThemeName2:string = ''
+  State:string = ''
+  city:string=''
+  UserDetails:UserData[] = []
+  stateName:string=''
+  
+  savedata(){
+    console.log("Saving data to backend");
+    var user = new UserData();
+    user.FirstName = this.udata.FirstName;
+    user.LastName = this.udata.LastName;
+    user.Location = this.udata.Location;
+    user.Company = this.udata.Company;
+
+    this.UserDetails.push(user);
+    console.log(this.UserDetails)
+    console.log(this.udata.FirstName);
+    console.log(this.udata.LastName);
+    this.udata =new UserData();
+  }
+  CheckData(event:Event){
+  console.log(this.FirstName.length);
 }
+  SendData(){
+    console.log("Sending data to backend");
+  }
+}
+
